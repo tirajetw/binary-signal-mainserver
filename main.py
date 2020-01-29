89ad21c46f7e786f86db9a12fc295e5fb721d3f6
JETTTT <tirajetw@JETTTTs-MacBook-Pro.local>
2020-01-30 02:29:52 +0700

    edit main func.

--- main.py
+++ main.py
@@ -17,16 +17,21 @@ currency_pair = {
 }
 
 browser = mechanicalsoup.StatefulBrowser(
-    soup_config = {'features': 'lxml'},
-    user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    )
+    soup_config={'features': 'lxml'},
+    user_agent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+)
+
 
 def check_winrate(action):
     browser.open("https://binary-signal.com/en/chart/" + action)
-    list_history = str(browser.get_current_page().find_all(class_="history")[0]).split('"')
-    win_index = [i for i, n in enumerate(list_history) if n == "win"] # List comprehension
-    list_history = str(browser.get_current_page().find_all(class_="history")[0]).split('"')
-    loss_index = [i for i, n in enumerate(list_history) if n == "loss"] # List comprehension
+    list_history = str(browser.get_current_page().find_all(
+        class_="history")[0]).split('"')
+    win_index = [i for i, n in enumerate(
+        list_history) if n == "win"]  # List comprehension
+    list_history = str(browser.get_current_page().find_all(
+        class_="history")[0]).split('"')
+    loss_index = [i for i, n in enumerate(
+        list_history) if n == "loss"]  # List comprehension
     data = loss_index + win_index
     data = list(data)
     data.sort()
@@ -34,26 +39,26 @@ def check_winrate(action):
     recommended_level = 0
     for x in data:
         try:
-            if (   x == win_index[0] 
-                or x == win_index[1] 
-                or x == win_index[2] 
-                or x == win_index[3] 
-                or x == win_index[4] 
-                or x == win_index[5] 
-                or x == win_index[6] 
-                or x == win_index[7]):
+            if (x == win_index[0]
+                or x == win_index[1]
+                or x == win_index[2]
+                or x == win_index[3]
+                or x == win_index[4]
+                or x == win_index[5]
+                or x == win_index[6]
+                    or x == win_index[7]):
                 result_data.append(1)
         except:
-            if ( x == loss_index[0] 
-                or x == loss_index[1] 
-                or x == loss_index[2] 
-                or x == loss_index[3] 
-                or x == loss_index[4] 
-                or x == loss_index[5] 
-                or x == loss_index[6] 
-                or x == loss_index[7]):
+            if (x == loss_index[0]
+                or x == loss_index[1]
+                or x == loss_index[2]
+                or x == loss_index[3]
+                or x == loss_index[4]
+                or x == loss_index[5]
+                or x == loss_index[6]
+                    or x == loss_index[7]):
                 result_data.append(0)
-    #recommend part
+    # recommend part
     j = 7
     sum_weight = 0
     for i in result_data:
@@ -62,7 +67,8 @@ def check_winrate(action):
     recommended_level = sum_weight/0.28
     recommended_level = int(recommended_level)
 
-    return result_data,recommended_level
+    return result_data, recommended_level
+
 
 def time_check():
     spinner = spinning_cursor()
@@ -72,8 +78,8 @@ def time_check():
         time.sleep(0.1)
         sys.stdout.write('\b')
     timeis = time.localtime()
-    #print(timeis)
-    cal = [0,15,30,45] # rev2 : change refresh time
+    # print(timeis)
+    cal = [0, 15, 30, 45]  # rev2 : change refresh time
     time.sleep(1)
     for x in range(4):
         if(int(timeis.tm_min) == cal[x]):
@@ -81,26 +87,28 @@ def time_check():
             return True
     return False
 
+
 def spinning_cursor():
     while True:
         for cursor in '|/-\\':
             yield cursor
 
-def listToString(s):  
-    
-    # initialize an empty string 
-    str1 = ""  
-    
-    # traverse in the string   
-    for ele in s:  
-        str1 += str(ele)   
-    
-    # return string   
-    return str1  
+
+def listToString(s):
+
+    # initialize an empty string
+    str1 = ""
+
+    # traverse in the string
+    for ele in s:
+        str1 += str(ele)
+
+    # return string
+    return str1
 
 
 ## RUNNING PART ##
-if(__name__=='__main__'):
+if(__name__ == '__main__'):
     status = []
     last_status = []
     print('Running bot.')
@@ -108,27 +116,20 @@ if(__name__=='__main__'):
         try:
             if time_check():
                 for action in currency_pair:
-                    data,level = check_winrate(action)
+                    data, level = check_winrate(action)
                     print(action, data, level)
                     if level >= 2:
-                        status.append(tuple((action,level)))
+                        status.append(tuple((action, level)))
                     # else:
                     #     status.append(0)
 
-                if status != last_status:
-                    last_status = status
-                    timeis = time.localtime()
-                    status.sort(key = operator.itemgetter(1), reverse = True)
-                    statusJson = json.dumps(status)
-                    print(statusJson)
-                    print('have updated.')
-                    pubMqtt.pubMQTT(statusJson)
-                    status.clear()
-                    time.sleep(60)
-                elif status == last_status:
-                    print('No update.')
-                    time.sleep(10)
-        
+                status.sort(key=operator.itemgetter(1), reverse=True)
+                statusJson = json.dumps(status)
+                print(statusJson)
+                print('Updated.')
+                pubMqtt.pubMQTT(statusJson)
+                status.clear()
+                time.sleep(10)
+
         except Exception as e:
             print(e)
-
